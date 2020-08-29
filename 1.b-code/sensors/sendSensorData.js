30f0dac699e4f38ae2f6a24cccfb2a1d0f4fb2cb
DamianMoralesM <damian_morales_utn@hotmail.com>
2020-08-29 12:51:04 -0300

    Added comments to code

--- 1.b-code/sensors/sendSensorData.js
+++ 1.b-code/sensors/sendSensorData.js
@@ -1,15 +1,14 @@
 const axios = require('axios').default;
 
-
-function sendSensorData(sensorId, unit) {
+function sendSensorData(sensorId) {
 
     const host = 'http://localhost:3000';
     const url = `${host}/sensors/${sensorId}`;
+    // random between 1 and 100
     let data = {
         value: Math.floor(Math.random() * 100) + 1,
     };
 
-    data.value = Math.floor(Math.random() * 100) + 1;
     console.log(`Data to be send: ${JSON.stringify(data)} from sensor ${sensorId}`)
     axios.post(url, data).then((response) => {
         console.log(response.data);
